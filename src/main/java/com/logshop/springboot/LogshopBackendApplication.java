5a30cdb9a0908630b8bb065e01526d9c7363d31d
kish_deemu <deemudk@gmail.com>
2021-06-07 23:13:09 +0530

    edit payment

--- src/main/java/com/logshop/springboot/LogshopBackendApplication.java
+++ src/main/java/com/logshop/springboot/LogshopBackendApplication.java
@@ -3,6 +3,7 @@ package com.logshop.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class LogshopBackendApplication {
 
